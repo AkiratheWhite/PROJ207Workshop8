7ca35cd8552349f26e9aef6e1893140dba39583d
chester solang <chester.jr02@gmail.com>
2021-05-13 12:15:50 -0600

    Full Display

--- app/src/main/java/com/example/travelexpertsapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/travelexpertsapp/ui/notifications/NotificationsFragment.kt
@@ -4,33 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import com.example.travelexpertsapp.R
+import com.example.travelexpertsapp.data.Customer
 import com.example.travelexpertsapp.data.Package
 import com.google.gson.Gson
-import java.util.ArrayList
+import java.util.*
+
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
                 ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val lvPackages= root.findViewById<ListView>(R.id.lvPackages)
+        val pkgName = root.findViewById<TextView>(R.id.pkgName)
+        val pkgStartDate = root.findViewById<TextView>(R.id.pkgStartDate)
+        val pkgEndDate = root.findViewById<TextView>(R.id.pkgEndDate)
+        val pkgDesc = root.findViewById<TextView>(R.id.pkgDesc)
+        val pkgBasePrice = root.findViewById<TextView>(R.id.pkgBasePrice)
+        val pkgAgencyCommission = root.findViewById<TextView>(R.id.pkgAgencyCommission)
+
+        lvPackages.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+            val c = lvPackages.getAdapter().getItem(position) as Package
+            //tvPackages.setText(c.packageDetail())
+            pkgName.setText(c.pkgName);
+            pkgStartDate.setText("Start Date: " + c.pkgStartDate);
+            pkgEndDate.setText("End Date: " + c.pkgEndDate);
+            pkgDesc.setText(c.pkgDesc);
+            pkgBasePrice.setText("Base Price: " + "$"+c.pkgBasePrice);
+            pkgAgencyCommission.setText("Commission: $" + c.pkgAgencyCommission);
+        })
 
         getPackages(lvPackages)
         return root
@@ -49,10 +68,21 @@ class NotificationsFragment : Fragment() {
                 val gson = Gson()
 
                 for (i in 0 until response.length()) {
-                    output.add(gson.fromJson(response.getJSONObject(i).toString(), Package::class.java))
+                    output.add(
+                        gson.fromJson(
+                            response.getJSONObject(i).toString(),
+                            Package::class.java
+                        )
+                    )
                 }
 
-                val listAdpt = context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, output) }
+                val listAdpt = context?.let {
+                    ArrayAdapter(
+                        it,
+                        android.R.layout.simple_list_item_1,
+                        output
+                    )
+                }
                 list.adapter = listAdpt
 
             },
@@ -61,5 +91,5 @@ class NotificationsFragment : Fragment() {
             }
         )
         mQueue.add(request)
-    }
-}
+    }//..getPackages
+}//class NotificationsFragment
